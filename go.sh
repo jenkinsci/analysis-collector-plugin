5b1b2c06f159a63bcaacd63aa2a771d1f72b19f8
Ulli Hafner <ullrich.hafner@gmail.com>
2011-07-06 20:32:10 +0200

    Update of launcher shell script.

--- go.sh
+++ go.sh
@@ -1,6 +1,7 @@
 rm -rf $HUDSON_HOME/plugins/analysis-collector*
 
-mvn install
+mvn install || { echo "Build failed"; exit 1; }
+
 cp -f target/*.hpi $HUDSON_HOME/plugins/
 cd $HUDSON_HOME
 java -jar jenkins.war
